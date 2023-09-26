d7c9cc04546113e0e016d4f6351a6786282e2a37
Yael Duckwen <yaelduckwen@gmail.com>
2023-09-26 16:01:18 -0300

    fuentes separadas + diseño

--- escrutinio-elecciones.php
+++ escrutinio-elecciones.php
@@ -1,14 +1,11 @@
 <?php
 /**
  * 
- * @link              https://quierohacertuweb.com
- * @since             1.0.0
- * @package           dolar_blue_hoy
  * 
  * Plugin Name:		Escrutinio Elecciones
  * Plugin URI:		https://quierohacertuweb.com
  * Description:		Muestra resultados de Elecciones.
- * Version:			0.1
+ * Version:			0.2
  * Author:			Yael Duckwen
  * Author URI:		https://quierohacertuweb.com
  *
@@ -54,7 +51,6 @@ function registrar_candidatos_custom_post_type() {
 
 /* Listado de Candidatos */
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-
 // Agregar columnas personalizadas a la tabla de CPT "Candidatos"
 function agregar_columnas_personalizadas($columns) {
 	$columns['partido'] = 'Partido';
@@ -90,55 +86,88 @@ add_action('manage_candidato_posts_custom_column', 'rellenar_columnas_personaliz
 
 
 
+/* Editor de Candidatos RAPIDO */
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+function editar_rapido_campo_votos($column_name, $post_type) {
+	if ($column_name === 'votos' && $post_type === 'candidato') {
+		?>
+		<fieldset class="inline-edit-col-left">
+			<div class="inline-edit-col">
+				<label for="votos" class="inline-edit-group">Votos (%):</label>
+				<input type="number" step="0.01" min="0" max="100" name="votos" class="votos" value="<?php echo esc_attr(get_post_meta($post_id, 'votos', true)); ?>">
+			</div>
+		</fieldset>
+		<?php
+	}
+}
+
+function guardar_valores_rapidos_votos($post_id) {
+	if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
+	if (!current_user_can('edit_post', $post_id)) return;
+
+	if (isset($_REQUEST['votos'])) {
+		update_post_meta($post_id, 'votos', floatval($_REQUEST['votos']));
+	}
+}
+
+// Agregar la edición rápida para el campo "Votos"
+add_action('quick_edit_custom_box', 'editar_rapido_campo_votos', 10, 2);
+add_action('save_post', 'guardar_valores_rapidos_votos');
+
+
 /* Editor de Candidatos */
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-
 /* Metabox Detalles */
 function agregar_metabox_detalles() {
 	add_meta_box(
-		'detalles_metabox',
+		'ee-metabox-detalles',
 		'Detalles',
 		'mostrar_metabox_detalles',
-        'candidato', /* CPT name */
-        'normal',
-        'high'
-    );
+		'candidato', /* CPT name */
+		'normal',
+		'high'
+	);
 }
 
 function mostrar_metabox_detalles($post) {
-    // Recuperar valores de los campos Partido, Votos y Puesto
+	/* Recuperar valores de los campos Partido, Votos y Puesto */
 	$partido = get_post_meta($post->ID, 'partido', true);
 	$votos = get_post_meta($post->ID, 'votos', true);
-    $puesto = get_post_meta($post->ID, 'puesto', true); // Nuevo campo "Puesto"
+	$puesto = get_post_meta($post->ID, 'puesto', true);
 
-    // Opciones para el campo select "Puesto"
-    $opciones_puesto = array(
-    	'intendente' => 'Intendente',
-    	'gobernador' => 'Gobernador',
-    	'presidente' => 'Presidente',
-    );
+	/* Opciones para el campo select "Puesto" */
+	$opciones_puesto = array(
+		'intendente' => 'Intendente',
+		'gobernador' => 'Gobernador',
+		'presidente' => 'Presidente',
+	);
 
-    // Mostrar el formulario del metabox
-    echo '<label for="partido">Partido:</label>';
-    echo '<input type="text" id="partido" name="partido" value="' . esc_attr($partido) . '" /><br />';
+	/* Mostrar el formulario del metabox */
+	echo '<div class="mb">';
+	echo '<label for="partido">Partido:</label>';
+	echo '<input type="text" id="partido" name="partido" value="' . esc_attr($partido) . '" />';
+	echo '</div>';
 
-    echo '<label for="votos">Votos (%):</label>';
-    echo '<input type="number" id="votos" name="votos" step="0.01" min="0" max="100" value="' . esc_attr($votos) . '" /><br />';
+	echo '<div class="mb">';
+	echo '<label for="puesto">Puesto:</label>';
+	echo '<select id="puesto" name="puesto">';
+	foreach ($opciones_puesto as $valor => $nombre) {
+		echo '<option value="' . esc_attr($valor) . '" ' . selected($puesto, $valor, false) . '>' . esc_html($nombre) . '</option>';
+	}
+	echo '</select>';
+	echo '</div>';
 
-    // Campo select "Puesto"
-    echo '<label for="puesto">Puesto:</label>';
-    echo '<select id="puesto" name="puesto">';
-    foreach ($opciones_puesto as $valor => $nombre) {
-    	echo '<option value="' . esc_attr($valor) . '" ' . selected($puesto, $valor, false) . '>' . esc_html($nombre) . '</option>';
-    }
-    echo '</select>';
+	echo '<div class="mb">';
+	echo '<label for="votos">Votos (%):</label>';
+	echo '<input type="number" id="votos" name="votos" step="0.01" min="0" max="100" value="' . esc_attr($votos) . '" />';
+	echo '</div>';
 }
 
 function guardar_metabox_detalles($post_id) {
 	if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
 	if (!current_user_can('edit_post', $post_id)) return;
 
-    // Guardar valores de los campos Partido, Votos y Puesto
+	// Guardar valores de los campos Partido, Votos y Puesto
 	if (isset($_POST['partido'])) {
 		update_post_meta($post_id, 'partido', sanitize_text_field($_POST['partido']));
 	}
@@ -164,11 +193,13 @@ add_action('save_post', 'guardar_metabox_detalles');
 function mostrar_candidatos_shortcode($atts) {
     // Establece los valores predeterminados para los atributos
 	$atts = shortcode_atts(array(
-        'eleccion' => '',
-        'mesas' => '',
-        'fuente' => '',
-        'actualizacion' => '',
-    ), $atts);
+		'eleccion' => '',
+		'mesas-intendente' => '',
+		'mesas-gobernador' => '',
+		'mesas-presidente' => '',
+		'fuente' => '',
+		'actualizacion' => '',
+	), $atts);
 
 	/* Configura los argumentos para la consulta basada en los atributos */
 	$args = array(
@@ -213,21 +244,29 @@ function mostrar_candidatos_shortcode($atts) {
 
     // Cargar y mostrar la plantilla personalizada
 	ob_start();
-    include plugin_dir_path(__FILE__) . 'templates/tabs.php';
-    return ob_get_clean();
+	include plugin_dir_path(__FILE__) . 'templates/tabs.php';
+	return ob_get_clean();
 }
 add_shortcode('mostrar_candidatos', 'mostrar_candidatos_shortcode');
 
 
 
-/* Añadir Bootstrap */
+/* Añadir Estilos */
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-function enqueue_bootstrap() {
+
+function cargar_estilo_admin() {
+	$css_url = plugins_url('ee-admin.css', __FILE__);
+	wp_enqueue_style('ee-estilo-admin', $css_url);
+}
+add_action('admin_enqueue_scripts', 'cargar_estilo_admin');
+
+function enqueue_ee_frontend_styles() {
+	$css_url = plugins_url('ee-style.css', __FILE__);
+	wp_enqueue_style('ee-estilo-frontend', $css_url);
 	wp_enqueue_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css');
 	wp_enqueue_script('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js', array('jquery'), '4.5.2', true);
 }
-
-add_action('wp_enqueue_scripts', 'enqueue_bootstrap');
+add_action('wp_enqueue_scripts', 'enqueue_ee_frontend_styles');
 
 function load_jquery() {
 	wp_enqueue_script('jquery');
--- templates/tabs.php
+++ templates/tabs.php
@@ -1,153 +1,119 @@
 <?php 
-$mesas = $atts['mesas'];
+$mesas_intendente = $atts['mesas-intendente'];
+$mesas_gobernador = $atts['mesas-gobernador'];
+$mesas_presidente = $atts['mesas-presidente'];
 $fuente = $atts['fuente'];
 $actualizacion = $atts['actualizacion'];
 
 /* Función de comparación personalizada para ordenar los candidatos por votos de mayor a menor */
-// function ordenar_por_votos($a, $b) {
-// 	return $b['votos'] - $a['votos'];
-// }
+function ordenar_por_votos($a, $b) {
+	return $b['votos'] - $a['votos'];
+}
 
 /* Ordenar los candidatos utilizando la función de comparación personalizada */
-//usort($candidatos, 'ordenar_por_votos');
+usort($candidatos, 'ordenar_por_votos');
 ?>
 <div class="candidatos-tabs">
-	<ul class="nav nav-pills  nav-fill" role="tablist">
+	<ul id="ee-nav-pills" class="nav nav-pills  nav-fill" role="tablist">
 		<li class="nav-item">
-			<a class="nav-link active" id="intendente-tab" data-toggle="tab" href="#intendente" role="tab" aria-controls="intendente" aria-selected="true">Intendente</a>
+			<a class="nav-link active" id="presidente-tab" data-toggle="tab" href="#presidente" role="tab" aria-controls="presidente" aria-selected="false">Presidente</a>
 		</li>
 		<li class="nav-item">
 			<a class="nav-link" id="gobernador-tab" data-toggle="tab" href="#gobernador" role="tab" aria-controls="gobernador" aria-selected="false">Gobernador</a>
 		</li>
 		<li class="nav-item">
-			<a class="nav-link" id="presidente-tab" data-toggle="tab" href="#presidente" role="tab" aria-controls="presidente" aria-selected="false">Presidente</a>
+			<a class="nav-link" id="intendente-tab" data-toggle="tab" href="#intendente" role="tab" aria-controls="intendente" aria-selected="true">Intendente</a>
 		</li>
+		
+		
 	</ul>
 	<div class="tab-content">
-		<div class="tab-pane fade show active" id="intendente" role="tabpanel" aria-labelledby="intendente-tab">
-			<h3 class="mt-3">Intendente</h3>
-			<div class="custom-candidates-grid">
+		<div class="tab-pane fade" id="intendente" role="tabpanel" aria-labelledby="intendente-tab">
+			<h3 class="mt-3 ee-puesto-title">Intendente</h3>
+			<div class="ee-candidates-grid">
 				<?php
 				foreach ($candidatos as $candidato) {
 					if ($candidato['puesto'] === 'intendente') {
 						echo '<article class="card">';
 						echo '<div class="candidato-cara">' . $candidato['imagen_destacada'] . '</div>';
 						echo '<div class="candidato-votos">' . esc_html($candidato['votos']) . '%</div>';
+						echo '<div class="candidato-data">';
 						echo '<div class="candidato-nombre">' . esc_html($candidato['nombre']) . '</div>';
 						echo '<div class="candidato-partido">' . esc_html($candidato['partido']) . '</div>';
+						echo '</div>';
 						echo '</article>';
 					}
 				}
 				?>
 			</div>
+
+			<div class="ee-fuentes mt-4">
+				<ul>
+					<li>Mesas escrutadas: <strong><?php echo esc_html($mesas_intendente); ?>%</strong></li>
+					<li>Fuente: <strong><?php echo esc_html($fuente); ?></strong></li>
+					<li>Actualización: <strong><?php echo esc_html($actualizacion); ?> hs</strong></li>
+				</ul>
+			</div>
 		</div>
 
-
 		<div class="tab-pane fade" id="gobernador" role="tabpanel" aria-labelledby="gobernador-tab">
-			<h3 class="mt-3">Gobernador</h3>
-			<div class="custom-candidates-grid">
+			<h3 class="mt-3 ee-puesto-title">Gobernador</h3>
+			<div class="ee-candidates-grid">
 				<?php
 				foreach ($candidatos as $candidato) {
 					if ($candidato['puesto'] === 'gobernador') {
 						echo '<article class="card">';
+						echo '<div class="candidato-cara">' . $candidato['imagen_destacada'] . '</div>';
 						echo '<div class="candidato-votos">' . esc_html($candidato['votos']) . '%</div>';
+						echo '<div class="candidato-data">';
 						echo '<div class="candidato-nombre">' . esc_html($candidato['nombre']) . '</div>';
 						echo '<div class="candidato-partido">' . esc_html($candidato['partido']) . '</div>';
+						echo '</div>';
 						echo '</article>';
 					}
 				}
 				?>
 			</div>
+			<div class="ee-fuentes mt-4">
+				<ul>
+					<li>Mesas escrutadas: <strong><?php echo esc_html($mesas_gobernador); ?>%</strong></li>
+					<li>Fuente: <strong><?php echo esc_html($fuente); ?></strong></li>
+					<li>Actualización: <strong><?php echo esc_html($actualizacion); ?> hs</strong></li>
+				</ul>
+			</div>
 		</div>
 
-		<div class="tab-pane fade" id="presidente" role="tabpanel" aria-labelledby="presidente-tab">
-			<h3 class="mt-3">Presidente</h3>
-			<div class="custom-candidates-grid">
+		<div class="tab-pane fade show active" id="presidente" role="tabpanel" aria-labelledby="presidente-tab">
+			<h3 class="mt-3 ee-puesto-title">Presidente</h3>
+			<div class="ee-candidates-grid">
 				<?php
 				foreach ($candidatos as $candidato) {
 					if ($candidato['puesto'] === 'presidente') {
 						echo '<article class="card">';
+						echo '<div class="candidato-cara">' . $candidato['imagen_destacada'] . '</div>';
 						echo '<div class="candidato-votos">' . esc_html($candidato['votos']) . '%</div>';
+						echo '<div class="candidato-data">';
 						echo '<div class="candidato-nombre">' . esc_html($candidato['nombre']) . '</div>';
 						echo '<div class="candidato-partido">' . esc_html($candidato['partido']) . '</div>';
+						echo '</div>';
 						echo '</article>';
 					}
 				}
 				?>
+			</div>
+			<div class="ee-fuentes mt-4">
+				<ul>
+					<li>Mesas escrutadas: <strong><?php echo esc_html($mesas_presidente); ?>%</strong></li>
+					<li>Fuente: <strong><?php echo esc_html($fuente); ?></strong></li>
+					<li>Actualización: <strong><?php echo esc_html($actualizacion); ?> hs</strong></li>
+				</ul>
 			</div>
 		</div>
 
 	</div>
 </div>
 
-<div>
-	<ul>
-		<li>Mesas escrutadas: <?php echo esc_html($mesas); ?> %</li>
-		<li>Fuente: <?php echo esc_html($fuente); ?></li>
-		<li>Actualización: <?php echo esc_html($actualizacion); ?> hs</li>
-	</ul>
-</div>
-
 <style type="text/css">
-	.custom-candidates-grid {
-		text-align: center;
-		display: grid;
-		gap: 1rem;
-		grid-template-columns: repeat(3,1fr);
-		/*
-		grid-template-columns: repeat(5,200px);
-		
-		text-transform: capitalize;
-		overflow-x: scroll;
-		overflow-y: hidden;
-		scroll-direction: horizontal;
-		-webkit-overflow-scrolling: touch;*/
-	}
-	.custom-candidates-grid .card {
-		padding: 1rem;
-		border: solid 1px;
-		border-radius: 5px;
-	}
-	.candidato-cara img {
-		max-width: 100px;
-		height: auto;
-		border-radius: 100px;
-	}
-	.candidato-votos {
-		font-size: 3rem;
-		font-weight: bold;
-	}
-	.candidato-nombre {
-		font-size: 1.5rem;
-		font-weight: bold;
-	}
-	.candidato-partido {
-		font-size: 1rem;
-	}
-
-	.custom-candidates-table {
-		width: 100%;
-		border-collapse: collapse;
-		text-transform: capitalize;
-	}
-
-	.custom-candidates-table th {
-		background-color: #f2f2f2;
-		text-align: left;
-		padding: 10px;
-	}
-
-	.custom-candidates-table tr:nth-child(even) {
-		background-color: #f2f2f2;
-	}
-
-	.custom-candidates-table tr:nth-child(odd) {
-		background-color: #ffffff;
-	}
-
-	.custom-candidates-table td {
-		padding: 10px;
-		border-bottom: 1px solid #ccc;
-	}
+	
 
 </style>
